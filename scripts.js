7a64b1ad3689b063575456d5281343c341d5c705
Cam <cameronmanzo111@gmail.com>
2024-04-11 20:39:09 -0400

    more

--- scripts.js
+++ scripts.js
@@ -6,9 +6,11 @@ for (i = 0; i < coll.length; i++) {
     this.classList.toggle("active");
     var content = this.nextElementSibling;
     if (content.style.display === "block") {
-      content.style.display = "none";
+      content.style.display = "none"
     } else {
       content.style.display = "block";
     }
   });
 }
+
+
